49f02ecfff39892968d19ec4c585147c4b9da177
Matheus Ferreira <matheus.fer33@gmail.com>
2022-03-28 21:55:37 -0300

    novos componentes e layouts, atualização da lógica

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+// Aqui o elemento filho header está alterando um estado no componente pai.
+//ele recebe uma função como props, e ela é acessada no onClick do butão.
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
     return (
       <Container>
           <Content>
             <img src={logoImg} alt="dt money" />
-            <button type="button">
+            <button onClick={onOpenNewTransactionModal} type="button">
                 Nova Transação
             </button>
+
+            
           </Content>
       </Container>
     )
